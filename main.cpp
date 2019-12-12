ebe9f526043dea8ed71daee8234df4cde546ff67
canott <56608418+canott@users.noreply.github.com>
2019-12-12 17:13:19 -0500

    main menu work

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int mainMenu(){
+	int choice;
+	cout <<"      Main Menu:     \n"
+		 <<"=====================\n"
+		 <<"1. Animal Information\n"
+		 <<"2. Add An Animal     \n"
+		 <<"3. Feeding Schedule  \n";
+	cin >>choice;
+	return choice;
+}
 
 int main(int argc, char** argv) {
+	string name;
+	string txtname;
+	if (mainMenu() == 1){
+		cout <<"Enter in Animal Name";
+		cin >>name;
+		txtname = name + ".txt";
+		
+	}
 	return 0;
 }
