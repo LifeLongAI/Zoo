bb6ed58067a666ece2ad1f3915ba4e3642339072
LifeLongDev <rawleycaleb@gmail.com>
2019-12-12 17:24:54 -0500

    Minor changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include <queue>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
 int main(int argc, char** argv) {
+	queue<string> Queue;
+	system("PAUSE");
 	return 0;
 }
