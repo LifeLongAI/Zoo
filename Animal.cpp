0fbdd12ef40a887f64e46b04bd4943582352b28a
andalite5 <andygiovanni5@gmail.com>
2019-12-12 17:22:17 -0500

    Animal implementation

--- Animal.cpp
+++ Animal.cpp
@@ -1,1 +1,59 @@
-#include <iostream>
+#include "Animal.h"
+
+Animal::Animal()
+{
+
+}
+
+string Animal::getFood()
+{
+    return food;
+}
+
+string Animal::getName()
+{
+    return name;
+}
+
+char Animal::getSex()
+{
+    return sex;
+}
+
+float Animal::getWeight()
+{
+    return weight;
+}
+
+void Animal::setFood(string reFood)
+{
+    food = reFood;
+}
+
+void Animal::setName(string mahName)
+{
+    name = mahName;
+}
+
+void Animal::setSex(char sexay)
+{
+    if(sexay == 'm' || sexay == 'f' || sexay == 'M' || sexay == 'F')
+    {
+        sex = sexay;
+    }
+}
+
+void Animal::setWeight(float chonker)
+{
+    weight = chonker;
+}
+
+string Animal::print()
+{
+    string ret = "";
+    ret += name + "\n";
+    ret += to_string(weight) + "\n";
+    ret += sex + "\n";
+    ret += food;
+    return ret;
+}
